c97ba1c3b33f9c3c83458e603dbe4351cfa51a64
joneyspark <sparkjoney@gmail.com>
2020-06-19 04:25:36 -0400

    Laravel components

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,15 @@
 @extends('shopify-app::layouts.default')
 
 @section('content')
-<h1>Dashboard</h1>
+<div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+    <div id="wrapper" class="container py-4 px-4 mx-auto">
+        <div class="sm:grid sm:h-32 sm:grid-flow-row sm:gap-4 sm:grid-cols-3">
+            <x-status type="positive" title="Today's Wishlist" number="32" growth="9" />
+            <x-status type="negative" title="Yesterday's Wishlist" number="20" growth="20" />
+            <x-status type="normal" title="Total Wishlist" number="430" growth="8" />
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('scripts')
